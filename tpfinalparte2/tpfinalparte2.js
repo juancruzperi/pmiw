1674acb9e08a9ee29780bb7cbcc95aff280d3c72
juancruzperi <108028598+juancruzperi@users.noreply.github.com>
2025-11-16 02:57:14 -0300

    Update tpfinalparte2.js

--- tpfinalparte2/tpfinalparte2.js
+++ tpfinalparte2/tpfinalparte2.js
@@ -1,6 +1,6 @@
 //Comision 1
 //Hanna Mendoza y Juan Cruz Peri
-//link a youtube:
+//link a youtube: https://www.youtube.com/watch?v=_9FgQCT8k6U
 let juego;
 let estado=[];
 let fondo, Annimg, Cecyimg, hechizo, portal, reloj;
@@ -43,4 +43,5 @@ function mouseClicked(){
 juego.estados();
 juego.sonido();
 }
+
 
